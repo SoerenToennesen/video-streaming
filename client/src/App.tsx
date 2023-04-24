1416b756930ccc6abe652a4c871bed2d90f89cbb
SoerenToennesen <sorentoennesen@gmail.com>
2023-04-24 18:13:01 +0200

    added server side video metadata api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { Route, BrowserRouter as Router} from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Home";
 import Player from "./Player";
 
 function App() {
   return (
-      <div className="App">
-          <header className="App-header">
-            <video controls muted>
-              <source src="http://localhost:4000/video" type="video/mp4"></source>
-            </video>
-          </header>
-      </div>
+      <Router>
+          <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/player/:id" element={<Player/>} />
+          </Routes>
+      </Router>
   )
 }
 
--- server/app.js
+++ server/app.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const cors = require("cors");
+
+const videos = [
+    {
+        id: 0,
+        poster: "/video/0/poster",
+        duration: "3 mins",
+        name: "Video 1",
+    },
+    {
+        id: 1,
+        poster: "/video/1/poster",
+        duration: "4 mins",
+        name: "Video 2",
+    },
+    {
+        id: 2,
+        poster: "/video/2/poster",
+        duration: "2 mins",
+        name: "Video 3",
+    },
+]
 
 const app = express();
 
 app.get("/video", (req, res) => {
     res.sendFile("assets/vid1.mp4", {root: __dirname});
 });
+
+app.use(cors());
+app.get("/videos", (req, res) => res.json(videos));
 
 app.listen(4000, () => {
     console.log("Listening on port " + 4000);
